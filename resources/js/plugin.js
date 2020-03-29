a5ea8414ca500852bb20a011a6a31db4dd8a347a
Husamettin Arslan <husamettinarslan@hotmail.com>
2020-03-29 23:07:27 +0200

    second stage

---
+++ resources/js/plugin.js
@@ -0,0 +1,20 @@
+let field = document.querySelector ('textarea');
+let backUp = field.getAttribute ('placeholder');
+let btn = document.querySelector ('.btn');
+let clear = document.getElementById ('clear');
+
+field.onfocus = function (){
+    this.setAttribute('placeholder', '');
+    this.style.borderColor = '#333';
+    btn.style.display = 'block'
+};
+
+field.onblur = function(){
+    this.setAttribute('placeholder',backUp);
+    this.style.borderColor = '#aaa'
+};
+
+clear.onclick = function(){
+    btn.style.display = 'none';
+    field.value = '';
+};
