db40c4e3e87330f615fe783058acffd06baf34ac
Husamettin Arslan <husamettinarslan@hotmail.com>
2020-03-30 00:00:21 +0200

    fetch

---
+++ resources/js/fetch.js
@@ -0,0 +1,25 @@
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+const ul = document.getElementById('Movies');
+const url = 'data/data.json';
+fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+        console.log(data.movies);
+        console.log("Visa första i json-objektet: " + data.movies[0].title);
+        let movies = data.movies;
+        return movies.map(function(movie) {
+            let li = createNode('li');
+            li.innerHTML = movie.title + " " + movie.director;
+            append(ul, li);
+        })
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
